30ff92bbf22fd51f926b5b3e31f198823dfeda3a
Lim <lim@limingdeMacBook-Pro.local>
2020-08-20 13:57:17 +0800

    feat: test react

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-// import Test from './pages/test'
-import HookDemo from './pages/hook'
+import Test from './pages/test'
+// import HookDemo from './pages/hook'
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Test/> */}
-      <header className="App-header">
-        <HookDemo/>
-      </header>
+      <Test/>
     </div>
   );
 }
--- react-test/src/pages/test.js
+++ react-test/src/pages/test.js
@@ -1,62 +1,17 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
 export default class Test extends Component {
-  static propTypes = {
-    value: PropTypes.number
-  };
-
-  state = {
-    value: ''
-  };
-
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
-      value: props.value,
       showMask: false,
     };
   }
-
-  componentWillMount() {
-    this.setState({value: '1'});
-  }
-
-  componentDidMount() {
-    this.setState({value: '2'});
-
-    window.queueMicrotask(() => {
-      this.setState({ showMask: true });
-    });
-
-    document.addEventListener(
-      'click',
-      () => {
-        this.setState({ showMask: false });
-      },
-      false
-    );
-  }
-
-  handleChange(e) {
-    // console.log(e.target.value);
-    if (e.target.value.length <= 5) {
-      this.setState({ value: e.target.value });
-    } else {
-      window.setTimeout(() => {
-        this.setState({ value: e.target.value });
-      }, 0);
-    }
-  }
-
-
+  
   render() {
-    const { value, showMask } = this.state;
-    console.log(value);
+    console.log('render 触发~~~');
     return (
-      <div className="input-wrap">
-        { showMask ? <div> DOM </div> : null }
-        Here：<input type="text" value={value} onChange={this.handleChange.bind(this)} />
+      <div>
+        <button onClick={()=>this.setState({ showMask: true })}>Click</button>
       </div>
     );
   }
